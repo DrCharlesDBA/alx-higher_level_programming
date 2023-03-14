93bb5f12df3a57a9be5cc58fc9b060cda76ee100
DrCharlesDBA <drcharlesdba@gmail.com>
2023-03-14 08:26:15 +0000

    fix the vi errors

--- 0x00-python-hello_world/5-print_string.py
+++ 0x00-python-hello_world/5-print_string.py
@@ -1,4 +1,4 @@
-vi #!/usr/bin/python3
+#!/usr/bin/python3
 str = "Holberton School"
 print(3 * str)
 print(str[:9])
--- 0x00-python-hello_world/6-concat.py
+++ 0x00-python-hello_world/6-concat.py
@@ -1,4 +1,4 @@
-vi #!/usr/bin/python3
+#!/usr/bin/python3
 str1 = "Holberton"
 str2 = "School"
 str1 += " " + str2
