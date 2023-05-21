ed983384dc98f64d5e2840c6bd085dba9d6c6225
DrCharlesDBA <drcharlesdba@gmail.com>
2023-05-21 04:29:43 -0700

    task 3

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for lowercase_alphabet in [chr(i) for i in range(97, 123)]:
-    if 'q' not in lowercase_alphabet and 'e' not in lowercase_alphabet:
-            print("{}".format(lowercase_alphabet), end="")
+for i in range(ord('a'), ord('z')+1):
+    if chr(i) not in ('q', 'e'):
+        print("{0}".format(chr(i)), end='')
