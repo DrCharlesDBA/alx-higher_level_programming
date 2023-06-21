4469c1cb32725f9fb8f0b9f00c2fe5740999381a
DrCharlesDBA <drcharlesdba@gmail.com>
2023-06-21 14:28:58 -0700

    1st ammended commit

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
-'''
-Write a function that returns the JSON representation
-of an object (string)
-'''
-
-
+"""Defines a string-to-JSON function."""
 import json
 
 
 def to_json_string(my_obj):
+    """Return the JSON representation of a string object."""
     return json.dumps(my_obj)
--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
-'''
-A function that returns an object (Python data structure)
-represented by a JSON string
-'''
-
-
+# 6-from_json_string.py
+"""Defines a JSON-to-object function."""
 import json
 
 
 def from_json_string(my_str):
+    """Return the Python object representation of a JSON string."""
     return json.loads(my_str)
