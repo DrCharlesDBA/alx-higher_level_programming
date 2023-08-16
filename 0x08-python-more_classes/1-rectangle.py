79ea020da951116d5e12383836c01f16aae74ff7
Charles <117086143+DrCharlesDBA@users.noreply.github.com>
2023-08-16 21:45:12 +0200

    1st commit warm up task

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -9,15 +9,7 @@ class Rectangle:
         """Initialize a new Rectangle.
 
         Args:
-            width (int): The width of the new rectangle.
-            height (int): The height of the new rectangle.
-        """
-        self.width = width
-        self.height = height
-
-    @property
-    def width(self):
-        """Get/set the width of the rectangle."""
+            width (inttr5    """Get/set the width of the rectangle."""
         return self.__width
 
     @width.setter
